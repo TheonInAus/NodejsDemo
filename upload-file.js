15063d267f3925a6ae7c6778a5b2cb3fc570a947
Theon <theon6208@gmail.com>
2024-07-17 19:42:53 +1000

    feat(upload): upload file

--- upload-file.js
+++ upload-file.js
@@ -19,7 +19,6 @@ http.createServer(function (req, res) {
       //  res.write('File uploaded and moved!');
       //  res.end();
       //})
-
     });
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
